f16380b15fdd8ec800a9edc41b8641857f2d5abf
Jay Thorat <j.thorat10@gmail.com>
2023-07-24 22:36:16 +0530

    final

--- src/TicTacToeGame/Board.jsx
+++ src/TicTacToeGame/Board.jsx
@@ -6,6 +6,7 @@ const Board = () => {
   const [state, setState] = useState(Array(9).fill(null));
   //   initializing first turn of X's
   const [isXTurn, setIsXTurn] = useState(true);
+  const [isTie,setIsTie] = useState(false);
 
   const checkWinner = () => {
     const winnerLogic = [
@@ -19,8 +20,6 @@ const Board = () => {
       [2, 4, 6],
     ];
 
-
-
     for (let logic of winnerLogic) {
       const [a, b, c] = logic;
       if (state[a] !== null && state[a] === state[b] && state[a] === state[c])
@@ -28,12 +27,12 @@ const Board = () => {
     }
     return false;
   };
-  const isWinner = checkWinner()
+  const isWinner = checkWinner();
 
   // Function to handle Square click
   const handleClick = (index) => {
-    if(state[index] !== null){
-        return;
+    if (state[index] !== null) {
+      return;
     }
     // Creating a copy of state then checking X turn or not to update X or O and then changing isXTurn
     const copyState = [...state];
@@ -41,36 +40,41 @@ const Board = () => {
     setState(copyState);
     setIsXTurn(!isXTurn);
     checkWinner();
-
-
   };
 
-  const playAgain= () => {
+  const playAgain = () => {
     setState(Array(9).fill(null));
-  }
+  };
+  
+
   return (
-    
     <div className="board-container">
-        
-        {isWinner ? <>{isWinner} won the game <button onClick={playAgain}>Play Again</button></>:
+      {isWinner ? (
         <>
-        <h3>{isXTurn ? "X's Turn" : "O's Turn" }</h3>
-      <div className="board-row">
-        <Square onClick={() => handleClick(0)} value={state[0]} />
-        <Square onClick={() => handleClick(1)} value={state[1]} />
-        <Square onClick={() => handleClick(2)} value={state[2]} />
-      </div>
-      <div className="board-row">
-        <Square onClick={() => handleClick(3)} value={state[3]} />
-        <Square onClick={() => handleClick(4)} value={state[4]} />
-        <Square onClick={() => handleClick(5)} value={state[5]} />
-      </div>
-      <div className="board-row">
-        <Square onClick={() => handleClick(6)} value={state[6]} />
-        <Square onClick={() => handleClick(7)} value={state[7]} />
-        <Square onClick={() => handleClick(8)} value={state[8]} />
-      </div>
-      </>}
+          {isWinner} won the game{" "};
+          <button onClick={playAgain}>Play Again</button>
+        </>
+      ) : (
+        <>
+          <h3>{isXTurn ? "X's Turn" : "O's Turn"}</h3>
+          <div className="board-row">
+            <Square onClick={() => handleClick(0)} value={state[0]} />
+            <Square onClick={() => handleClick(1)} value={state[1]} />
+            <Square onClick={() => handleClick(2)} value={state[2]} />
+          </div>
+          <div className="board-row">
+            <Square onClick={() => handleClick(3)} value={state[3]} />
+            <Square onClick={() => handleClick(4)} value={state[4]} />
+            <Square onClick={() => handleClick(5)} value={state[5]} />
+          </div>
+          <div className="board-row">
+            <Square onClick={() => handleClick(6)} value={state[6]} />
+            <Square onClick={() => handleClick(7)} value={state[7]} />
+            <Square onClick={() => handleClick(8)} value={state[8]} />
+          </div>
+          <button onClick={playAgain}>RESET</button>
+        </>
+      )}
     </div>
   );
 };
